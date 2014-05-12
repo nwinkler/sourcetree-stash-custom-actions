cb009e64f3271b774c25ec1e2b21273f03f13473
Nils Winkler <nils@nilswinkler.com>
2014-05-12 09:57:40 +0200

    Added support for Windows execution using some really ugly PowerShell and Batch hacking. Largely untested due to lack of a full Windows environment.

--- Stash/stash_base.bat
+++ Stash/stash_base.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%stash_base.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' '%~1'";
+
+rem Ugly hack to set the GitUrl variable from the command output
+for /f "delims=" %%i in ('git config --get remote.origin.url') do set GitUrl=%%i
+
+rem Run PowerShell with the provided Git URL, output the result to stdout
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' '%GitUrl%'";
