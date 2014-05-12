dd01f831d0f8b428f900ec98fd9b9e05a970d70f
Nils Winkler <nils@nilswinkler.com>
2014-05-12 11:43:05 +0200

    Fixed path handling to point to the script's base directory.

--- Stash/copy_commit_url_windows.bat
+++ Stash/copy_commit_url_windows.bat
@@ -2,7 +2,7 @@
 set GitHash=%~1
 
 rem Ugly hack to set the WebGitUrl variable from the command output
-for /f "delims=" %%i in ('stash_base.bat') do set WebGitUrl=%%i
+for /f "delims=" %%i in ('%~dp0/stash_base.bat') do set WebGitUrl=%%i
 
 rem Use the Windows 'clip' utility to copy to the clipboard
 echo %WebGitUrl%/commits/%GitHash% | clip
