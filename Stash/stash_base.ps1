f445fd48637b8f179a28c5069f0e0d025182a291
Nils Winkler <nils@nilswinkler.com>
2014-05-12 09:57:40 +0200

    Added support for Windows execution using some really ugly PowerShell and Batch hacking. Largely untested due to lack of a full Windows environment.

--- Stash/stash_base.ps1
+++ Stash/stash_base.ps1
@@ -1,5 +1,7 @@
 param(
 [string]$a
 )
+# The first parameter of this script is assigned to the $a variable.
 
+# Use PowerShell RegEx to change the URL and get rid of the '.git' part
 $a -replace "(.*)/scm/(.*)/(.*)\.git", '$1/projects/$2/repos/$3'
