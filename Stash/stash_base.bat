ec735f4b8619a0ca949f05678566c3a13f983724
Nils Winkler <nils@nilswinkler.com>
2014-05-12 15:10:41 +0200

    Rewrote windows file to use batch substitution and path manipulation instead of PowerShell. PowerShell wasn't working from within SourceTree.

--- Stash/stash_base.bat
+++ Stash/stash_base.bat
@@ -1,9 +1,25 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%stash_base.ps1
+@echo off
+setlocal enableDelayedExpansion
 
-rem Ugly hack to set the GitUrl variable from the command output
-for /f "delims=" %%i in ('git config --get remote.origin.url') do set GitUrl=%%i
+for /f "delims=" %%i in ('git config --get remote.origin.url') do set sourceUrl=%%i
+rem set sourceUrl=https://services.sungard.com/git/scm/html5/html5-framework-core-components.git
 
-rem Run PowerShell with the provided Git URL, output the result to stdout
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' '%GitUrl%'";
+set "baseUrl=%sourceUrl%"
+set "baseUrl=!baseUrl:.git=!"
+
+for /f "delims=" %%U in ("\%baseUrl%\..\..\..") do (
+    set "base=%%~pnxU"
+    set "base=!base:~1!"
+    set "base=!base:\=/!"
+    set "base=!base:https:/=https://!"
+)
+
+for /f "delims=" %%U in ("\%baseUrl%\..") do (
+    set "project=%%~nxU"
+)
+
+for /f "delims=" %%U in ("\%baseUrl%") do (
+    set "repo=%%~nxU"
+)
+
+echo %base%/projects/%project%/repos/%repo%
